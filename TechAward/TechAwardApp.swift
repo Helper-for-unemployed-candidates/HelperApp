c0c338b640e77a9077e08dedfc031dfbc432c7f2
justCod1ng <iutjourney@gmail.com>
2024-07-27 15:24:33 +0500

    Initial Commit

---
+++ TechAward/TechAwardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechAwardApp.swift
+//  TechAward
+//
+//  Created by justCod1ng on 27/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TechAwardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
